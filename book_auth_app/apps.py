935c0010c3dd86ac51d4b7d0505efe693959a4e3
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-27 21:20:03 +0300

    init sub

---
+++ book_auth_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookAuthAppConfig(AppConfig):
+    name = 'book_auth_app'
---
+++ book_auth_app/models.py
@@ -0,0 +1,18 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    notes = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    decs = models.TextField(null=True)
+    authors = models.ManyToManyField(Author, related_name="books")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
